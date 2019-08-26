360657b92ac25ef63e71ab2173e33670eceb57c3
楊宗翰 <feveraly@gmail.com>
2019-08-26 12:51:43 +0800

    add camera roll button

--- src/components/AddFoodCamera.vue
+++ src/components/AddFoodCamera.vue
@@ -1,7 +1,11 @@
 <template>
     <div id="add-food-camera">
-        <p>add food camera</p>
-        <a href="line://nv/camera">open camera</a>
+        <div class="row justify-content-center" style="margin-top: 80px;">
+            <div class="col-4 open-camera-button"><a href="line://nv/camera">拍攝照片</a></div>
+            <div class="col-1"></div>
+            <div class="col-4 open-camera-button"><a href="line://nv/cameraRoll/single">相簿上傳</a></div>
+        </div>
+        
     </div>
 </template>
 
@@ -19,4 +23,23 @@ export default {
 #add-food-camera {
     margin-top: 30px;
 }
+
+.open-camera-button {
+    padding-top: 5px;
+    padding-bottom: 5px;
+    border-radius: 10px;
+    background-color: gray;
+    text-decoration: none;
+}
+
+.open-camera-button > a{
+    text-decoration: none;
+    color: white;
+}
+
+.open-camera-button > a:hover{
+    text-decoration: none;
+    color: white;
+}
+
 </style>
