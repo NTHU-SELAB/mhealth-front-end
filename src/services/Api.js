b076c1d2ac9b3e82b214376f6065723d60338dcf
楊宗翰 <feveraly@gmail.com>
2019-08-27 17:52:37 +0800

    server to https

--- src/services/Api.js
+++ src/services/Api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export default () => {
     return axios.create({
-        baseURL: `http://mhealth.feveral.idv.tw/mhealth/`,
+        baseURL: `https://mhealth.feveral.idv.tw/mhealth/`,
     })
 }
 
