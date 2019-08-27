3ceedcbe5d550b68ba02ce3cc806dd87f7beee85
楊宗翰 <feveraly@gmail.com>
2019-08-27 17:01:03 +0800

    add food manually

--- src/services/FoodService.js
+++ src/services/FoodService.js
@@ -1,21 +1,21 @@
 import Api from '@/services/Api.js'
-
+import qs from 'qs'
 
 export default {
 
     async postFoodManually(userID, foodName, foodWeight, calorie, fat, sugar, fiber, meal) {
-        const response = await Api().post('food/recordFood.php', {
+        const response = await Api().post('food/recordFood.php', qs.stringify({
             'record': 'record',
             'userID': userID,
-            'name': name,
+            'name': foodName,
             'weight': foodWeight,
             'calorie': calorie,
             'fat': fat,
             'sugar': sugar,
             'fiber': fiber,
             'meal': meal,
-            'recordTime': Date.now()/1000
-        })
+            'recordTime': new Date(Date.now())
+        }))
         return response
     },
 
