802c0af6a8ab78f0e559f145082bf717eca2576e
楊宗翰 <feveraly@gmail.com>
2019-09-17 18:38:55 +0800

    add restaurant page

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import MainPage from '@/pages/MainPage.vue'
 import AddFoodPage from '@/pages/AddFoodPage.vue'
+import RestaurantPage from '@/pages/RestaurantPage.vue'
 
 Vue.use(Router)
 
@@ -18,6 +19,11 @@ export default new Router({
                     path: '/add-food',
                     name: 'add-food-page',
                     component: AddFoodPage,
+                },
+                {
+                    path: '/restaurant',
+                    name: 'restaurant-page',
+                    component: RestaurantPage,
                 }
             ]
         }
