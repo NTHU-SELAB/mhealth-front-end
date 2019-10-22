f05974fcb92ab20239b6d6b66fd17af910f540bf
楊宗翰 <feveraly@gmail.com>
2019-10-22 19:25:10 +0800

    open camera test android

--- src/components/AddFoodCamera.vue
+++ src/components/AddFoodCamera.vue
@@ -2,33 +2,34 @@
     <div id="add-food-camera">
 
        <div class="row justify-content-center" v-if="!this.$route.query.image" style="margin-top: 80px;">
-            <div class="col-4 open-camera-button"><a href="line://nv/camera">拍攝照片{{this.$route.query.id}}</a></div>
+            <div class="col-4 open-camera-button"><a href="#" @click="openCamera()">拍攝照片{{this.$route.query.id}}</a></div>
             <div class="col-1"></div>
             <div class="col-4 open-camera-button"><a href="line://nv/cameraRoll/single">相簿上傳</a></div>
         </div>
 
-        
         <div  class="row justify-content-center" v-if="this.$route.query.image" style="margin-top: 30px;">
             <div class="card col-11" style="width: 18rem;">
-              <img class="card-img-top" src="https://i.imgur.com/k8OgO79.png" alt="Card image cap">
-              <!--image holder-->
-              <div class="card-body">
-                 <select class="browser-default custom-select col-6" style="border:2px #ccc solid; border-radius:20px 0px 0px 20px;">
-                     <option selected>辨識結果</option>
-                     <option value="1">麵包</option>
-                     <option value="2">三明治</option>
-                     <option value="3">吐司</option>
-                 </select>
-                 <select class="browser-default custom-select col-6" style="border:2px #ccc solid; border-radius:0px 20px 20px 0px;" v-model="foodName" @change="FoodonChange()">
-                     <option selected>{{foodName}}</option>
-                     <option v-for="list in lists" :key="list.value">{{list.item}}</option>
-                 </select>
-             </div>
-             <!--food select-->
-               <ul class="list-group list-group-flush">
-                   <li class="list-group-item">
-                       <label>重量:{{foodweight}}(g) /熱量:{{Math.round(foodweight*foodcalori*100) /100}}(kcal)</label>
-                       <input type="range" class="custom-range" min="0" max="500" v-model="foodweight">
+                <img class="card-img-top" src="https://i.imgur.com/k8OgO79.png" alt="Card image cap">
+                
+                <!--image holder-->
+                <div class="card-body">
+                    <select class="browser-default custom-select col-6" style="border:2px #ccc solid; border-radius:20px 0px 0px 20px;">
+                        <option selected>辨識結果</option>
+                        <option value="1">麵包</option>
+                        <option value="2">三明治</option>
+                        <option value="3">吐司</option>
+                    </select>
+                    <select class="browser-default custom-select col-6" style="border:2px #ccc solid; border-radius:0px 20px 20px 0px;" v-model="foodName" @change="FoodonChange()">
+                        <option selected>{{foodName}}</option>
+                        <option v-for="list in lists" :key="list.value">{{list.item}}</option>
+                    </select>
+                </div>
+
+                <!--food select-->
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">
+                        <label>重量:{{foodweight}}(g) /熱量:{{Math.round(foodweight*foodcalori*100) /100}}(kcal)</label>
+                        <input type="range" class="custom-range" min="0" max="500" v-model="foodweight">
                     </li>
                     <li id="food-info" class="list-group-item">
                         <ul><label>脂肪:{{Math.round(foodweight*foodfat*100) /100}}(g)</label></ul>
@@ -42,12 +43,9 @@
                         <button class="col-5 btn btn-primary " style="border:2px #ccc solid; border-radius:20px;" type="submit" @click="cancelCamera()">取消</button>
                     </li>
                     <!--submit button-->
-               </ul>
-           </div>
-           
+                </ul>
+            </div>
         </div>
-
-        
     </div>
 
 
@@ -80,22 +78,24 @@ export default {
     },
     methods: {
         cancelCamera() {
-            console.log("cancel camera add food")
             liff.closeWindow();
+        },
+
+        openCamera() {
+            window.location.href = "line://nv/camera"
         },
 
         addFoodRecord() {
             alert("新增飲食成功")
         },
 
-        FoodonChange() {
+        foodonChange() {
             console.log("food change")
             this.foodcalori=4.45;
             this.foodprotein=0.125;
             this.foodfat=0.06;
             this.foodcarbon=0.85;
-        }
-
+        },
     }
 
 }
