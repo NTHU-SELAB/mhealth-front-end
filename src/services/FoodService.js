401ae17de16402fef656be505baf506b57aa57c6
楊宗翰 <feveraly@gmail.com>
2019-10-23 19:59:30 +0800

    solved add-food api bug

--- src/services/FoodService.js
+++ src/services/FoodService.js
@@ -2,21 +2,21 @@ import Api from '@/services/Api.js'
 
 export default {
 
-    // async postFoodManually(userID, foodName, foodWeight, calorie, fat, sugar, fiber, meal) {
-    //     const response = await Api().post('food/recordFood.php', qs.stringify({
-    //         'record': 'record',
-    //         'userID': userID,
-    //         'name': foodName,
-    //         'weight': foodWeight,
-    //         'calorie': calorie,
-    //         'fat': fat,
-    //         'sugar': sugar,
-    //         'fiber': fiber,
-    //         'meal': meal,
-    //         'recordTime': new Date(Date.now())
-    //     }))
-    //     return response
-    // },
+    async postFoodManually(userID, foodName, foodWeight, calorie, fat, sugar, fiber, meal) {
+        const response = await Api().post('food/recordFood.php', qs.stringify({
+            'record': 'record',
+            'userID': userID,
+            'name': foodName,
+            'weight': foodWeight,
+            'calorie': calorie,
+            'fat': fat,
+            'sugar': sugar,
+            'fiber': fiber,
+            'meal': meal,
+            'recordTime': new Date(Date.now())
+        }))
+        return response
+    },
 
     async getFoodsByKeyword(keyword) {
         const response = await Api().get(`food?keyword=${keyword}`)
