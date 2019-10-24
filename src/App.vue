2f06c8b694a29c3addaa2ef6e61e4c45fedc32aa
Chen-hua <welkin172@gmail.com>
2019-10-24 16:47:13 +0800

    merge 1024-1647

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,3 @@
-<<<<<<< HEAD
-<template>
-  <div id="app">
-    <router-view></router-view>
-  </div>
-</template>
-
-<script>
-
-export default {
-  name: 'app',
-  components: {
-    
-  }
-}
-</script>
- 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 10px;
-}
-</style>
-=======
 <template>
   <div id="app">
     <router-view></router-view>
@@ -52,4 +24,3 @@ export default {
   color: #2c3e50;
 }
 </style>
->>>>>>> cbf7592b729c36e19e8df33e38a894b9a7a72343
