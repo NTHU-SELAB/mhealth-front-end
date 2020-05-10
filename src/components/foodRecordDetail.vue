150cd1e740ceb5fb1d0cadb8ace54120fe014e7b
楊宗翰 <feveraly@gmail.com>
2020-05-11 01:00:43 +0800

    food calender demo

---
+++ src/components/foodRecordDetail.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="modal fade" id="staticBackdrop" data-backdrop="static" tabindex="-1" role="dialog" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+        <div class="modal-header">
+            <h5 class="modal-title" id="staticBackdropLabel">4 / 20 晚餐 ： 牛排</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+        <div class="modal-body">
+            <div class="row">
+                <div class="col-6 food-record-detail">
+                    <div>時段：晚餐</div>
+                    <div>名稱：牛排</div>
+                    <div>重量：250g</div>
+                </div>
+                <div class="col-6 food-record-detail">
+                    <div>脂肪：33g</div>
+                    <div>卡路里：553 大卡
+                    </div>
+                    <div>膳食纖維：0g</div>
+                    <div>碳水化合物：0g</div>
+                </div>
+                <div class="col-12 food-record-detail">記錄時間：4 / 20 18:06</div>
+            </div>
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Understood</button>
+        </div>
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scope>
+.food-record-detail {
+    text-align: left;
+    font-size: 14px;
+}
+</style>
--- src/pages/FoodCalendarPage.vue
+++ src/pages/FoodCalendarPage.vue
@@ -9,9 +9,9 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav mr-auto">
                     <li class="nav-item active">
-                        <a class="nav-link" href="#">首頁<span class="sr-only">(current)</span></a>
+                        <a class="nav-link" href="/">首頁<span class="sr-only">(current)</span></a>
                     </li>
-                    <li class="nav-item active">
+                    <li class="nav-item active btn-primary" style="border-radius: 10px;">
                         <a class="nav-link" href="#">食物日誌</a>
                     </li>
                     <li class="nav-item active">
@@ -24,48 +24,61 @@
                         <a class="nav-link" href="#">聯絡我們</a>
                     </li>
                 </ul>
-                <form class="form-inline my-2 my-lg-0">
-                <input class="form-control mr-sm-2" type="search" placeholder="輸入文字以搜尋..." aria-label="Search">
-                <button class="btn btn-outline-light my-2 my-sm-0" type="submit">搜尋</button>
+                <form class="form-inline">
+                    <input class="form-control mr-sm-2" type="search" placeholder="輸入文字以搜尋..." aria-label="Search">
+                    <button class="btn btn-outline-light" type="submit">搜尋</button>
                 </form>
+                <img class="ml-2" src="https://i7.pngguru.com/preview/527/663/825/logo-person-user-person-icon.jpg"
+                        style="max-width: 3%; border-radius: 20px;">
+                <!-- <div class="navbar-right"><img src="https://i7.pngguru.com/preview/527/663/825/logo-person-user-person-icon.jpg" alt="" style="max-width: 100%;"></div> -->
             </div>
         </nav>
         <div class="container">
+            <div class="spinner-border text-primary" role="status" v-if="isloading" style="margin-top: 200px;">
+                <span class="sr-only">Loading...</span>
+            </div>
             <GChart style="height:210px;width:100%;"
                 type="LineChart"
+                v-if="!isloading"
+                class="food-calender-content"
+                :style="{opacity: contentOpacity}"
                 :data="chartContent()"
                 :options="chartOptions"
                 :resizeDebounce="500"
                 ref="chart"/>
-            <div class="row">
-                <div class="col-6 col-sm-3 col-lg-2 mb-3" v-for="record in 30" v-bind:key="record.image_url">
-                    <div class="card">
-                        <div class="pl-4 pr-4 pt-4 pb-4"><img src="../assets/food-icon.png" class="card-img-top food-icon"></div>
+            <div class="row food-calender-content" :style="{opacity: contentOpacity}">
+                <div class="col-6 col-sm-3 col-lg-2 mb-3" v-for="record in foodRecords" v-bind:key="record.image_url">
+                    <div class="card" data-toggle="modal" data-target="#staticBackdrop">
+                        <div class="pl-4 pr-4 pt-4 pb-4"><img :src="record.image_url" class="card-img-top food-icon"></div>
                         <div class="card-body">
-                            <h5 class="card-title" style="margin-bottom: 3px;">香酥雞腿飯</h5>
-                            <p class="card-text" style="margin-bottom: 3px; font-weight: bold; color: #8e9191;">4/20 晚餐</p>
-                            <p class="card-text" style="color: red;margin-bottom: 3px;">673大卡</p>
+                            <h5 class="card-title" style="margin-bottom: 3px;">{{record.name}}</h5>
+                            <p class="card-text" style="margin-bottom: 3px; font-weight: bold; color: #8e9191;">{{record.date}} {{record.meal}}</p>
+                            <p class="card-text" style="color: red;margin-bottom: 3px;">{{record.calorie}}大卡</p>
                             <!-- <a href="#" class="btn btn-primary">查看明細</a> -->
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-
+        <food-record-detail></food-record-detail>
     </div>
 </template>
 
 <script>
 import { GChart } from 'vue-google-charts'
 import foodCalendarData from '../mock/foodCalendarData.json'
+import foodRecordDetail from '@/components/foodRecordDetail.vue'
 
 export default {
     components: {
-        GChart
+        GChart,
+        foodRecordDetail
     },
 
     data() {
         return {
+            isloading: true,
+            contentOpacity: 0,
             foodRecords: [],
             chartDataHeader: ['Time', '卡路里'],
             chartData: [],
@@ -79,6 +92,10 @@ export default {
     mounted() {
         this.initChartData()
         this.foodRecords = foodCalendarData
+        setTimeout(() => {
+            this.isloading = false;
+            this.contentOpacity = 100;
+        }, 1300)
     },
 
     methods: {
@@ -95,8 +112,14 @@ export default {
 }
 </script>
 
-<style>
+<style scope>
 .food-icon {
     max-width: 100%;
+}
+
+.food-calender-content {
+    transition-property: opacity;
+    transition-duration: 1s;
+    transition-timing-function: ease-in-out;
 }
 </style>
