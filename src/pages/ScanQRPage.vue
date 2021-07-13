3c31e543de5f9dfbade8ae4dd31cdc8fdb6933af
TryMi <dcfvgb123456@gmail.com>
2021-07-13 20:58:15 +0800

    try to fix not working error

--- src/pages/ScanQRPage.vue
+++ src/pages/ScanQRPage.vue
@@ -1,22 +1,32 @@
 <template>
 <div id="scan-qr-page" class="container">
+  <span id="demo-span">{{ qr_string }}</span>
 </div>
 </template>
 
 <script>
+import liff from '@line/liff'
+
 export default {
   data() {
-      return {
-        qr_string: "qrcode"
-      }
+    return {
+      qr_string: "qrcode"
+    }
   },
 
   mounted() {
+    liff.init({ liffId: 'myLiffId' });
+
+    // alert("liff.scanCode: " + liff.scanCode)
     if (liff.scanCode) {
-      liff.scanCode().then((result) => {
-        qr_string = result.value
-        alert("QR Code: " + qr_string)
+      liff.scanCode().then(result => {
+        this.qr_string = JSON.stringify(result)
+      }).catch(err => {
+        this.qr_string = err.stringify()
       });
+    }
+    else {
+      alert(liff.scanCode)
     }
   }
 }
