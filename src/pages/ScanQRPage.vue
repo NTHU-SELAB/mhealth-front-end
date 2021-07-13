092fdc8a13fba376ab52557a160fcd2fc1b66889
TryMi <dcfvgb123456@gmail.com>
2021-07-13 16:41:38 +0800

    try to fix not working error

--- src/pages/ScanQRPage.vue
+++ src/pages/ScanQRPage.vue
@@ -1,6 +1,5 @@
 <template>
 <div id="scan-qr-page" class="container">
-  <span>just a useless message</span>
 </div>
 </template>
 
@@ -13,13 +12,12 @@ export default {
   },
 
   mounted() {
-    // if (liff.scanCode) {
-    //   liff.scanCode().then((result) => {
-    //     qr_string = result.value
-    //     alert("QR Code: " + qr_string)
-    //   });
-    // }
-    alert("QR Code: " + this.qr_string)
+    if (liff.scanCode) {
+      liff.scanCode().then((result) => {
+        qr_string = result.value
+        alert("QR Code: " + qr_string)
+      });
+    }
   }
 }
 </script>
