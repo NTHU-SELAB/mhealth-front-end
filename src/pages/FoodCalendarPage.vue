a621b023a92c286276b3652095c3646987646f06
lioumorgan <asasadwarfda1101@gmail.com>
2021-10-25 18:08:24 +0800

    修復食物日誌

--- src/pages/FoodCalendarPage.vue
+++ src/pages/FoodCalendarPage.vue
@@ -93,7 +93,7 @@ export default {
         async countCalorieDay(){
             const cals = []
             this.userID = await LiffService.getUserId()
-            for(let i=0; i<days; i++){
+            for(let i=0; i<this.days; i++){
                 let time = new Date(this.year,this.month,i+1);
                 let totalCalorie = 0;
                 this.foodRecords = await FoodService.getFoodRecordsByDay(this.userID,time);
