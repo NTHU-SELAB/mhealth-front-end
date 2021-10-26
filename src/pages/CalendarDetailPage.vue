b15a324c51d092b8e8bd351eeb36fa49a089f680
lioumorgan <asasadwarfda1101@gmail.com>
2021-10-26 18:32:49 +0800

    FIX

--- src/pages/CalendarDetailPage.vue
+++ src/pages/CalendarDetailPage.vue
@@ -97,7 +97,7 @@ export default {
     },
     methods: {
         async refreshMealList(){
-            let time = new Date(this.$route.params.year,this.$route.params.month-1,this.$route.params.day+1).getTime();
+            let time = new Date(this.$route.params.year,this.$route.params.month-1,this.$route.params.day).getTime();
             const userID = await LiffService.getUserId()
             this.mealList = await FoodService.getFoodRecordsByDay(userID,time);
             //console.log(time);
