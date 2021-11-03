086ac51e090e2e75289f1a06a15f3876981bf96f
lioumorgan <asasadwarfda1101@gmail.com>
2021-11-03 21:11:04 +0800

    FIX

--- src/main.js
+++ src/main.js
@@ -12,18 +12,20 @@ import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false
 
-router.beforeEach((to, from, next)=>{
-  const isLogin = localStorage.getItem('token') == 'ImLogin' ;
-  if( isLogin ){
-    next();
-  } else {
-    if( to.path !== '/login' && to.path !== '/food-calendar' && to.path !== '/add-food-manually'
-     && to.path !== '/add-food-camera' && to.path !== '/food-record' && to.path !== '/calendar-detail/:year/:month/:day')
-      next('/login');
-    else
+router.beforeEach(function (to, from, next) {
+    const isLogin = localStorage.getItem('token') == 'ImLogin'
+    if (isLogin) {
       next();
-  }
-});
+    } else {
+      if (to.path !== '/login' && to.path !== '/food-calendar' && to.path !== '/add-food-manually'
+        && to.path !== '/add-food-camera' && to.path !== '/food-record' && to.path !== '/calendar-detail/:year/:month/:day'){
+        next('/login');
+      }
+      else{
+        next();
+      }
+    }
+  });
 
 new Vue({
   router,
